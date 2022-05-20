85ba11d7502e2bf6064de66b30cf93e49d427fd7
Choi Ji Hun <100457604+Apocachip@users.noreply.github.com>
2022-05-20 09:45:29 +0900

    add

--- app7.py
+++ app7.py
@@ -35,15 +35,23 @@ def main() :
 
         # 파일명을 유니크하게 만들어서 저장해야 한다.
         # 현재시간을 활용해서, 파일명을 만든다.
-        current_time = datetime.now()
-        print(current_time.isoformat().replace(':', '_'))
-
-        new_filename = current_time.isoformat().replace(':', '_') + '.jpg'
-        
-        upload_file.name = new_filename
-        save_uploaded_file('temp', upload_file)
+            current_time = datetime.now()
+            print(current_time.isoformat().replace(':', '_'))
+            new_filename = current_time.isoformat().replace(':', '_') + '.jpg'
+            upload_file.name = new_filename
+            save_uploaded_file('temp', upload_file)
     elif choice == menu[1] :
         st.subheader('CSV 파일 업로드')
+        upload_file = st.file_uploader('CSV 파일 업로드', type=['csv'])
+        # 업로드 파일이 있으면 처리
+        if upload_file is not None :
+            # 파일명을 유니크하게 만들어서 저장해야 한다.
+            # 현재시간을 활용해서, 파일명을 만든다.
+            current_time = datetime.now()
+            print(current_time.isoformat().replace(':', '_'))
+            new_filename = current_time.isoformat().replace(':', '_') + '.csv'
+            upload_file.name = new_filename
+            save_uploaded_file('temp', upload_file)
     else :
         st.subheader('파일 업로드 프로젝트 입니다.')
 
